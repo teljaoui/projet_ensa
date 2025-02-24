68916eebb2d857462d0616e1065f8b1d8a72562d
teljaoui <teljaoui2004@gmail.com>
2025-02-24 09:23:14 +0000

    up

--- resources/views/admin/layout/asidebar.blade.php
+++ resources/views/admin/layout/asidebar.blade.php
@@ -8,24 +8,24 @@
 </div>
 <ul class="sidebar-nav">
     <li class="sidebar-item">
-        <a href="{{ route('admin') }}" class="sidebar-link">
+        <a href="{{ route('admin') }}" class="sidebar-link {{request()->is('admin') ? 'active' : ''}}">
             <i class="fa-solid fa-calendar-week"></i> <span>Réservation</span>
         </a>
     </li>
     <li class="sidebar-item">
-        <a href="{{ route('profliste') }}" class="sidebar-link">
+        <a href="{{ route('profliste') }}" class="sidebar-link {{request()->is('admin/prof*') ? 'active' : ''}}">
             <i class="fa-solid fa-person-chalkboard"></i>
             <span>Professeur</span>
         </a>
     </li>
     <li class="sidebar-item">
-        <a href="{{route("salles")}}" class="sidebar-link">
+        <a href="{{route("salles")}}" class="sidebar-link {{request()->is('admin/salles*') ? 'active' : ''}}">
             <i class="fa-solid fa-chalkboard-user"></i>
             <span>Salle</span>
         </a>
     </li>
     <li class="sidebar-item">
-        <a href="{{route("horaires")}}" class="sidebar-link">
+        <a href="{{route("horaires")}}" class="sidebar-link {{request()->is('admin/horaire*') ? 'active' : ''}}">
             <i class="fa-solid fa-calendar-days"></i>
             <span>Gestion des Horaires</span>
         </a>
